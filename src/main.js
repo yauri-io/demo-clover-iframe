27f256262c749a7c8ef1642e2dd2c9012bea5198
y-io <me@yauri.io>
2020-11-12 02:20:23 +0800

    add bugsnag

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginVue from '@bugsnag/plugin-vue'
+
+Bugsnag.start({
+    apiKey: '4384355ff980cd801ed3e2eae61b2e22',
+    plugins: [new BugsnagPluginVue()]
+})
 import App from './App.vue'
 import './index.css'
 import router from "./router";
